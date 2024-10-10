fadc769be83fcbe4bb2f4db211b6d48cc52c9845
Alejo Rossia <alejo.r94@gmail.com>
2024-10-10 14:49:14 +0200

    Added cp (phi^6) to the basis
    Added the H^6 operator to the printed basis. SMEFiT can fit this operator since the inclusion of the HH measurements/projections.
    Additionally, reorganized the folders and stored sample runcards in a folder. All the cards there are for the EW quadruplets models.

---
+++ sample_cards/InvarsFit_1LoopMatching_Model_Phi_4_12_32_custo_Mass_4_1Loop.py
@@ -0,0 +1,10 @@
+import numpy as np
+from utils import inspect_model
+
+MODEL_SPECS = dict(id="Phi_4_12_32_custo", collection="1LoopMatching", mass=4, pto="NLO", eft="NHO" )
+
+
+def inv1(results):
+	wwC\[CurlyPhi]B = results.wwC\[CurlyPhi]B
+	return np.abs(wwC\[CurlyPhi]B)
+
--- sample_cards/InvarsFit_1LoopMatching_Model_Phi_4_12_Mass_4_1Loop.py
+++ sample_cards/InvarsFit_1LoopMatching_Model_Phi_4_12_Mass_4_1Loop.py
@@ -4,3 +4,7 @@ from utils import inspect_model
 MODEL_SPECS = dict(id="Phi_4_12", collection="1LoopMatching", mass=4, pto="NLO", eft="NHO" )
 
 
+def inv1(results):
+	lamPhi = results.lamPhi
+	return np.abs(lamPhi)
+
