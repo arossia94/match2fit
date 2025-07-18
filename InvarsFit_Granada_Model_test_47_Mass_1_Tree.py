c6f3c6010e8e6656194abb0219cd62d965c4675d
Alejo Rossia <alejo.r94@gmail.com>
2025-07-18 11:32:03 +0200

    Added Granada Tree Level models - Beta version
    SM yukawas to be corrected.

---
+++ InvarsFit_Granada_Model_test_47_Mass_1_Tree.py
@@ -0,0 +1,15 @@
+import numpy as np
+from utils import inspect_model
+
+MODEL_SPECS = dict(id="test_47", collection="Granada", mass=1, pto="NLO", eft="NHO" )
+
+
+def inv1(results):
+	lamT1f3 = results.lamT1f3
+	match2fit`Private`\[Alpha]$10465[1] = results.match2fit`Private`\[Alpha]$10465[1]
+	return -0.25*(lamT1f3**2*match2fitxPrivatex\[Alpha]$10465(1))
+
+def inv2(results):
+	lamT1f3 = results.lamT1f3
+	return List(np.abs(lamT1f3))
+
